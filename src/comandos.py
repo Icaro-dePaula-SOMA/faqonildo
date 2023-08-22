8017a341572b9ab0e8b1a2a9212acf42ab15f2dc
somaicaro <icaro.paula@somasolucoes.com>
2023-08-22 17:30:43 -0300

    feat: adicionar conexao oracle e realizar busca do faq desejado

--- src/comandos.py
+++ src/comandos.py
@@ -1,13 +1,13 @@
 from src.faqbot import bot
+from src.ora import cur
 import asyncio
 
 def configurar():
     @bot.command()
     async def faq(ctx, nome_faq):
-        # cur = con_oracle
-        # cur.execute(f"select desc from faq where nome = {nome_faq}")
-        # desc = cur.fetchall()[0]
+        cur.execute(f"select descricao from faq where TITULO = {nome_faq}")
 
-        # await ctx.send(desc)
+        query = cur.fetchall()[0][0]
+
         await asyncio.sleep(1) 
         await ctx.send(nome_faq)
---
+++ src/ora.py
@@ -0,0 +1,8 @@
+import oracledb
+
+
+connection = oracledb.connect(user ='WNS', password = 'AD03AR02', dsn = '192.168.30.200/ORCL', port = 1521)
+
+cur = connection.cursor()
+
+
