635ef0b9b18329481b3f0dfd6695171157210f71
somaicaro <icaro.paula@somasolucoes.com>
2023-08-29 17:31:48 -0300

    feat: alterar dinamica do comando +faq, FAQ's com nomes parecidos tem seus nomes listados apenas

--- src/comandos.py
+++ src/comandos.py
@@ -2,21 +2,40 @@ from src.faqbot import bot
 from src.ora import cur
 import asyncio
 
+
+def select(faq_solicitado):
+    query = f"select titulo, descricao from faq where titulo = '{faq_solicitado}' or lower(titulo) like '%{str(faq_solicitado).lower()}%'"
+    cur.execute(query)
+    return cur.fetchall()
+
+
 def configurar():
     @bot.command()
     async def faq(ctx, faq_solicitado):
 
-        query = f"select titulo, descricao_old from faq where TITULO = '{faq_solicitado}' or titulo like '%{faq_solicitado}%'"
-
-        cur.execute(query)
-
+        query = select(faq_solicitado)
         await asyncio.sleep(1) 
 
-        query = cur.fetchall()
-        
-        if (not len(query)):
-            await ctx.send(f"Não encontrei nenhum FAQ chamado {faq_solicitado}, tem certeza que o nome é esse?")
-               
-        for faq in range(0, len(query)):
-            await ctx.send(f'```\n{query[faq][0]}:\n{query[faq][1]}\n```')
+        tam_query = len(query)
+
+        match tam_query:
+            case 0:
+                await ctx.send(f'Não encontrei nenhum FAQ chamado "{faq_solicitado}", tem certeza que o nome é esse?\n')
+
+            case 1:
+                await ctx.send(f"""
+{query[0][0]}\n
+{query[0][1]}
+### +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n""")
+            
+        if(tam_query > 1):
+
+            lista = ['']
+            
+            for faq in range(0, 19):
+                lista[0] += ((query[faq][0]) + '\n')
+            
+            await ctx.send(f"""
+Não encontrei o FAQ desejado, mas aqui vão alguns possíveis FAQ's para te ajudar:\n
+```{lista[0]}```""")
 
---
+++ src/formata_texto.py
@@ -0,0 +1,9 @@
+def formata(texto):
+    '*Itálico*'
+    '__*Sublinhado itálico*__'
+    '**Negrito**'
+    '__**Sublinhado negrito**__'
+    '***Negrito Itálico***'
+    '__***Sublinhado negrito e itálico***__'
+    '__Sublinhado__	'
+    '~~Riscado~~'
