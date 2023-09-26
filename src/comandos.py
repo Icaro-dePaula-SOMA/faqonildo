ceb3a481f33b757c4fdacdcf84e1f30be5c465bd
somaicaro <icaro.paula@somasolucoes.com>
2023-09-26 10:33:21 -0300

    feat: adicionar slash_command de busca para realizar a busca por faq's

--- src/comandos.py
+++ src/comandos.py
@@ -1,27 +1,49 @@
 from src.faqbot import bot
-from src.funcoes import separar, select, tem_faq, vazia
+from src.funcoes import vazia, separar, select, tem_faq
 import asyncio
 
 
-def configurar():
+def configurar():   
     @bot.command()
     async def faq(ctx, busca = ''):
-        if vazia(busca):
-            await ctx.send('Sua pesquisa está vazia, tenta denovo por favor.')
+        await asyncio.sleep(1)
 
-        busca = separar(busca)
+        if(not vazia(busca)):
+            busca = separar(busca)
 
-        faq = select(busca)
+            faq = select(busca)
 
-        resposta = tem_faq(faq, busca)
+            resposta = tem_faq(faq, busca) 
 
-        await asyncio.sleep(1) 
+            for i in range(0, len(resposta), 1800):
+                parte = resposta[i:i + 1800]
+                await ctx.send(parte)
+            return      
 
-        for i in range(0, len(resposta), 1800):
-            parte = resposta[i:i + 1800]
-            await ctx.send(parte)
+        await ctx.send('Sua pesquisa está vazia, tenta denovo por favor.')
 
 
-    # @slash.slash(name="sla", description="Teste 1")
-    # async def sla(ctx):
-    #     await ctx.send('Bd!')
+    @bot.slash_command(name='busca', description='Procure por um FAQ', )
+    async def busca(ctx, faqe):
+        await asyncio.sleep(1)
+
+        if(not vazia(faqe)):
+            busca = separar(faqe)
+
+            faqe = select(busca)
+
+            resposta = tem_faq(faqe, busca) 
+
+            for i in range(0, len(resposta), 1800):
+                parte = resposta[i:i + 1800]
+                await ctx.respond(parte)
+            return
+        
+        await ctx.respond('Sua pesquisa está vazia, tenta denovo por favor.')
+        
+
+    # @bot.slash_command(name='ja_pode', description='Já pode almoçar?', )
+    # async def busca(ctx):
+    #     await asyncio.sleep(1)
+
+    #     await ctx.respond('Ainda nn')
