82842e0657eb7789f79f9fe621351f1b809147a0
Icaro-dePaula-SOMA <icaro.paula@somasolucoes.com>
2024-06-10 08:40:17 -0300

    refactor: melhorar a legibilidade e a lógica das funções

--- src/funcoes.py
+++ src/funcoes.py
@@ -1,35 +1,39 @@
 from src.ora import cur
 import re
 
-
-def vazia(busca):
-    if busca.isspace() or len(busca) == 0:
-        return 1
-
-
-def separar(busca):
-    busca = busca.replace(',', ' ').split()
-    busca = '}% and %{'.join(busca)
-    busca = '%{' + busca + '}%'
-
-    return busca
+def validar(palavras_chave):
+    try: 
+        palavras_chave = int(palavras_chave)   
+    except:
+        if ((not palavras_chave.isspace()) or (len(palavras_chave) > 0)):
+            return 1     
+    else:
+        return 0
 
 
-def juntar(busca):
+def separar_palavras_chave(palavras_chave):
+    palavras_chave = palavras_chave.replace(',', ' ').split()
+    palavras_chave = '}% and %{'.join(palavras_chave)
+    palavras_chave_separadas = '%{' + palavras_chave + '}%'
+    
+    return palavras_chave_separadas
+
+
+def juntar_palavras_chave(busca):
     busca = re.sub('}% and %{', ', ', busca)
     busca = busca[2:-2]
 
     return busca
 
 
-def select(busca):
+def selecionar_faq(palavras_chave_separadas):
     try:
         query = f"""with aux as (
     select titulo, descricao from faq where cd_faq = (
         SELECT CD_FAQ FROM (
-                            SELECT CD_FAQ, CONTAINS(XML_FAQ, '{busca}') score
+                            SELECT CD_FAQ, CONTAINS(XML_FAQ, '{palavras_chave_separadas}') score
                             FROM V_XML_FAQ
-                            WHERE CONTAINS(XML_FAQ, '{busca}') > 0
+                            WHERE CONTAINS(XML_FAQ, '{palavras_chave_separadas}') > 0
                             order by score desc
                         )
         FETCH FIRST 1 ROW ONLY)
@@ -38,19 +42,13 @@ def select(busca):
     select titulo, F_EDITA_DESC_FAQ(descricao) from aux"""
         
         cur.execute(query)
-
         return cur.fetchone()
     
     except:
         return None
 
 
-def tem_faq(faq, busca):
-
-    if(faq == None):
-        busca = juntar(busca)
-        return f"""\nNão encontrei nenhum FAQ contendo "{busca}". Tenta denovo por favor."""
-
-    return f"""
-### {faq[0]}\n
-{faq[1]}"""
+def formatar_faq(faq_selecionado):
+    return f"""### 
+{faq_selecionado[0]}\n
+{faq_selecionado[1]}"""
