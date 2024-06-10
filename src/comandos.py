2c624a9f9894dbb936c09b7d96f685ea873d5cd2
Icaro-dePaula-SOMA <icaro.paula@somasolucoes.com>
2024-06-10 08:41:22 -0300

    refactor: melhorar a legibilidade e a lógica do comando faq

--- src/comandos.py
+++ src/comandos.py
@@ -1,49 +1,37 @@
 from src.faqbot import bot
-from src.funcoes import vazia, separar, select, tem_faq
-import asyncio
+from src.funcoes import validar, separar_palavras_chave, selecionar_faq, formatar_faq, juntar_palavras_chave
+import discord, asyncio
 
+def configurar():       
+    @bot.event
+    async def on_ready():
+        await bot.tree.sync()
 
-def configurar():   
-    @bot.command()
-    async def faq(ctx, busca = ''):
-        await asyncio.sleep(1)
+    @bot.tree.command(description="Procure por um FAQ")
+    async def faq(interact:discord.Interaction, palavras_chave:str):
+        try:
+            await asyncio.sleep(1)
+            if(validar(palavras_chave)):
+                palavras_chave_separadas = separar_palavras_chave(palavras_chave)
 
-        if(not vazia(busca)):
-            busca = separar(busca)
+                faq = selecionar_faq(palavras_chave_separadas)
 
-            faq = select(busca)
+                if faq == None:
+                    msg_sem_faq = f"""\nNão encontrei nenhum FAQ contendo "{juntar_palavras_chave(palavras_chave_separadas)}" :thinking: Tenta denovo por favor."""
+                    await interact.response.send_message(msg_sem_faq)
 
-            resposta = tem_faq(faq, busca) 
+                prefixo = f'{interact.user.name}, encontrei um FAQ que pode te ajudar :smiley: \n'         
+                resposta = prefixo + formatar_faq(faq) 
 
-            for i in range(0, len(resposta), 1800):
-                parte = resposta[i:i + 1800]
-                await ctx.send(parte)
-            return      
+                await interact.response.send_message(resposta[0:1800])
 
-        await ctx.send('Sua pesquisa está vazia, tenta denovo por favor.')
+                for i in range(1800, len(resposta), 1800):
+                    fragmento = resposta[i:i + 1800]
+                    await interact.followup.send(fragmento)
+            
+            await interact.response.send_message('Não consigo procurar por números :confused: tenta digitar algum texto.')
 
-
-    @bot.slash_command(name='busca', description='Procure por um FAQ', )
-    async def busca(ctx, faqe):
-        await asyncio.sleep(1)
-
-        if(not vazia(faqe)):
-            busca = separar(faqe)
-
-            faqe = select(busca)
-
-            resposta = tem_faq(faqe, busca) 
-
-            for i in range(0, len(resposta), 1800):
-                parte = resposta[i:i + 1800]
-                await ctx.respond(parte)
-            return
-        
-        await ctx.respond('Sua pesquisa está vazia, tenta denovo por favor.')
-        
-
-    # @bot.slash_command(name='ja_pode', description='Já pode almoçar?', )
-    # async def busca(ctx):
-    #     await asyncio.sleep(1)
-
-    #     await ctx.respond('Ainda nn')
+        except:
+            if not interact.response.is_done():
+                await interact.response.send_message('Tive alguns problemas com as palavras chave, tenta denovo por favor :grimacing:')
+                
